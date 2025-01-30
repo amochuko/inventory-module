b8902ab02a3e7557de4aaf4f747cd1877e85564a
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2025-01-30 15:19:28 +0100

    update: add character model

---
+++ src/model/character.ts
@@ -0,0 +1,16 @@
+import { Episode } from "../utils/types";
+
+export default class Character {
+  public id: string;
+
+  constructor(
+    public name: string,
+    public age: number,
+    public episode: Episode
+  ) {
+    this.name = name;
+    this.age = age;
+    this.id = name + "-" + Math.ceil((Math.random() * 100) / 10);
+    this.episode = episode;
+  }
+}
