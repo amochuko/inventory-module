3646c72709b67b06dd8ffd7473c05f47ec2e936b
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2025-02-01 21:29:20 +0100

    update: load modules and set PubSub

--- src/graphql/modules/appModules.ts
+++ src/graphql/modules/appModules.ts
@@ -1,7 +1,17 @@
 import { createApplication } from "graphql-modules";
+import { PubSub } from "graphql-subscriptions";
+import { infoModule } from "./infoModule/infoModule";
+import { userModule } from "./userModule";
+import { feedModule } from "./feedModule/feedModule";
 
 export const appModules = createApplication({
-  modules: [],
+  modules: [infoModule, userModule, feedModule],
+  providers: [
+    {
+      provide: PubSub,
+      useValue: new PubSub(),
+    },
+  ],
 });
 
 //TODO: Reaserch the benefits of Dataloader
