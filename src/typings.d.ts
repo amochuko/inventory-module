f0fcb668c0cef710742a4dc8af620dab914c9226
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2025-02-01 18:22:57 +0100

    update: extend global context

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,10 @@ import { GraphQLContext } from "./graphql/context/graphqlContext";
 declare module "graphql-yoga" {
   interface YogaInitialContext extends GraphQLContext {}
 }
+
+
+declare global {
+  namespace GraphQLModules {
+    interface GlobalContext extends GraphQLContext {}
+  }
+}
