bba947171115d58c4d76e7ba60fb38c58c94000c
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2025-02-03 07:55:49 +0100

    update: extend Context

--- src/graphql/context/graphqlContext.ts
+++ src/graphql/context/graphqlContext.ts
@@ -1,17 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 import { YogaInitialContext } from "graphql-yoga";
+import { FeedService } from "../modules/feed/feed.service";
 
-export type GraphQLContext = {
-  prisma: PrismaClient;
-  apiKey?: string | null;
-  jwt?: string | null
-};
+/**
+ * This the custom `GraphQLContext` used
+ * for in the resolvers of
+ */
+export interface GraphQLContext
+  extends YogaInitialContext,
+    GraphQLModules.GlobalContext {
+  readonly feedService: FeedService;
+  readonly apiKey?: string | null;
+  readonly jwt?: string | null;
+}
 
-export async function graphQLContext(
-  ctx: YogaInitialContext
-): Promise<GraphQLContext> {
+/**
+ * This function accepts as param `YogaInitialContext`
+ * and returns the update `Context`
+ * @param ctx YogaInitialContext
+ * @returns Promise<GraphQLContext>
+ */
+export async function graphQLContext(ctx: YogaInitialContext) {
   return {
-    prisma: new PrismaClient(),
+    feedService: new FeedService(new PrismaClient()),
     apiKey: ctx.request.headers.get("api-key"),
+    ...ctx,
   };
 }
