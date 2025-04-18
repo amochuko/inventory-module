9f585e7876148b639733027b893f4ec7d4263a87
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-18 16:28:24 +0100

    fix: clean up

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,20 @@
-import 'reflect-metadata'
 import { Server } from "http";
-import { debug } from "node:util";
-import { yoga } from "./graphql/yoga";
-import app from "./app";
 import { createServer } from "node:http";
+import { debug } from "node:util";
+import "reflect-metadata";
+import { yoga } from "./graphql/yoga";
 
 const PORT = process.env.PORT || 4000;
 
-const server: Server = createServer(yoga);;
+const server: Server = createServer(yoga);
 server.listen(PORT, () => {
   console.log(`App listening on port http://localhost:${PORT}`);
   console.info(`Server is running on http://localhost:${PORT}/graphql`);
 });
 
-server.once('close', async()=>{
-  await yoga.dispose()
-  
-})
+server.once("close", async () => {
+  await yoga.dispose();
+});
 
 process.on("SIGTERM", () => {
   debug("SIGTERM signal received: closing HTTP server.");
