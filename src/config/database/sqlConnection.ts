6e1c1f86fc213ca8da508686a32f2ca619c1f8ed
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-18 16:04:17 +0100

    fix: update db config

---
+++ src/config/database/sqlConnection.ts
@@ -0,0 +1,38 @@
+import pg from "pg";
+import env from "../env";
+
+export const dbClient =
+  process.env.NODE_ENV != "production"
+    ? new pg.Pool({
+        host: env.PGHOST_LOCAL,
+        user: env.PGUSER_LOCAL,
+        database: env.PGDATABASE_LOCAL,
+        password: env.PGPASSWORD_LOCAL,
+        port: env.PGPORT_LOCAL,
+        ssl: false,
+      })
+    : new pg.Pool({
+        connectionString: env.PGDB_CONNECTION_STRING,
+      });
+
+dbClient.on("error", (err) => {
+  console.log("Unexpected error on idle client: ", err);
+  process.exit(-1);
+});
+
+type SQLArgs = {
+  query: string;
+  params: (string | string[] | Buffer | ArrayBuffer)[];
+};
+export async function sql(args: SQLArgs) {
+  const client = await dbClient.connect();
+
+  try {
+    const result = await client.query(args.query, args.params);
+    return result;
+  } catch (err) {
+    throw err;
+  } finally {
+    client.release();
+  }
+}
