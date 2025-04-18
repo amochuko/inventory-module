ae11a334e3f39582c6b54545a10e0f12e1fe2dd6
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-18 16:10:54 +0100

    update: add close event

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
+import 'reflect-metadata'
 import { Server } from "http";
 import { debug } from "node:util";
-
+import { yoga } from "./graphql/yoga";
 import app from "./app";
+import { createServer } from "node:http";
 
-let server: Server;
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
 
-server = app.listen(port, () => {
-  console.log(`App listening on port http://localhost:${port}`);
+const server: Server = createServer(yoga);;
+server.listen(PORT, () => {
+  console.log(`App listening on port http://localhost:${PORT}`);
+  console.info(`Server is running on http://localhost:${PORT}/graphql`);
 });
+
+server.once('close', async()=>{
+  await yoga.dispose()
+  
+})
 
 process.on("SIGTERM", () => {
   debug("SIGTERM signal received: closing HTTP server.");
