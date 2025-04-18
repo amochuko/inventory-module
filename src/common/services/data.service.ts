7547965a5ff4dc5f00be4b5907298b1dd7745a0b
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-18 16:44:07 +0100

    fix:  clean up

--- src/common/services/data.service.ts
+++
@@ -1,9 +0,0 @@
-import "reflect-metadata";
-import { Injectable } from "graphql-modules";
-
-@Injectable()
-export class DataService {
-  age() {
-    return `23 years old`;
-  }
-}
--- src/common/utils/getCurrentDirectoryPath.ts
+++
@@ -1,5 +0,0 @@
-import path from 'node:path'
-
-export const getCurrentDirectoryPath = () => {
-  return path.resolve(__dirname);
-};
