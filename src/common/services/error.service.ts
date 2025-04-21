d1c7e036ddf28b0fe3db58de052e6c670c9ce662
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-21 11:21:53 +0100

    update: add ErrorService

---
+++ src/common/services/error.service.ts
@@ -0,0 +1,65 @@
+import { GraphQLError } from "graphql";
+import ValidationError from "../../error/validation.error";
+import {
+  CategoryCreationError,
+  CategoryNotFoundError,
+  DuplicateCategoryError,
+} from "../../graphql/modules/category/category.error";
+
+export class ErrorService {
+  static toGraphQLError(err: unknown): GraphQLError {
+    if (err instanceof GraphQLError) return err;
+
+    if (err instanceof ValidationError) {
+      return new GraphQLError(err.message, {
+        extensions: {
+          code: "VALIDATION_ERROR",
+          http: {
+            status: 400,
+          },
+        },
+      });
+    }
+
+    if (err instanceof DuplicateCategoryError) {
+      return new GraphQLError(err.message, {
+        extensions: {
+          code: "DUPLICATE_CATEGORY",
+          http: {
+            status: 409,
+          },
+        },
+      });
+    }
+
+    if (err instanceof CategoryNotFoundError) {
+      return new GraphQLError(err.message, {
+        extensions: {
+          code: "CATEGORY_NOT_FOUND",
+          http: {
+            status: 404,
+          },
+        },
+      });
+    }
+    if (err instanceof CategoryCreationError) {
+      return new GraphQLError(err.message, {
+        extensions: {
+          code: "CATEGORY_CREATION_ERROR",
+          http: {
+            status: 400,
+          },
+        },
+      });
+    }
+
+    console.error("Unhandled Error ->", err);
+
+    return new GraphQLError("Internal Server Error", {
+      extensions: {
+        code: "INTERNAL_SERVER_ERROR",
+        http: { status: 500 },
+      },
+    });
+  }
+}
