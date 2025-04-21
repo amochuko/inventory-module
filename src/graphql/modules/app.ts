01d0cea51dde8ef5dd58aa8bdd8a364876a50b17
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-21 11:17:34 +0100

    fix: remove unused module

--- src/graphql/modules/app.ts
+++ src/graphql/modules/app.ts
@@ -1,11 +1,10 @@
 import { createApplication } from "graphql-modules";
 import { PubSub } from "graphql-subscriptions";
-import { userModule } from "./user/user.module";
-import { feedModule } from "./feed/feed.module";
+import { categoryModule } from "./category/category.module";
 
 // This is the application, it contains GraphQL schema, and its implementation
 export const application = createApplication({
-  modules: [userModule, feedModule],
+  modules: [categoryModule],
   providers: [
     {
       provide: PubSub,
