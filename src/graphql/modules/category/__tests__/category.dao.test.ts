06b4b6d9d4fedd31068e59a2ee761b4805b16703
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-21 10:48:42 +0100

    update: add test for .findAll

--- src/graphql/modules/category/__tests__/category.dao.test.ts
+++ src/graphql/modules/category/__tests__/category.dao.test.ts
@@ -1,3 +1,4 @@
+import exp from "constants";
 import { sql } from "../../../../config/database/sqlConnection";
 import CategoryDAO from "../category.dao";
 
@@ -5,19 +6,26 @@ jest.mock("../../../../config/database/sqlConnection", () => ({
   sql: jest.fn(),
 }));
 
+const mockCategories = [
+  {
+    name: "Electronics",
+    abbrevCode: "ELE",
+    description: "Tech",
+  },
+  {
+    name: "Food",
+    abbrevCode: "FOD",
+    description: "Feed house",
+  },
+];
+
 describe("CategoryDAO", () => {
   const dao = new CategoryDAO();
 
   it("should create a category successfully", async () => {
-    const mockCategory = {
-      name: "Electronics",
-      abbrevCode: "ELE",
-      description: "Tech",
-    };
-
     (sql as jest.Mock).mockReturnValue({
       rowCount: 1,
-      rows: [mockCategory],
+      rows: mockCategories,
     });
 
     const result = await dao.create({
@@ -26,7 +34,7 @@ describe("CategoryDAO", () => {
       name: "Electronic",
     });
 
-    expect(result).toEqual(mockCategory);
+    expect(result).toEqual(mockCategories[0]);
   });
 
   it("should throw a creation error if insert fails", async () => {
@@ -37,13 +45,22 @@ describe("CategoryDAO", () => {
     ).rejects.toThrow("Failed to create category");
   });
 
-  it('should detect duplicate constraint violation',async ()=>{
+  it("should detect duplicate constraint violation", async () => {
     (sql as jest.Mock).mockRejectedValue(
       new Error(
         'duplicate key value violates unique constraint "categories_name_key"'
       )
     );
 
-    await expect(dao.create({name:'Duplicate', abbrevCode:'DUP',description:'Copy'})).rejects.toThrow('Category name already exists')
-  })
+    await expect(
+      dao.create({ name: "Duplicate", abbrevCode: "DUP", description: "Copy" })
+    ).rejects.toThrow("Category name already exists");
+  });
+
+  it("should return categories with .findAll()", async () => {
+    (sql as jest.Mock).mockResolvedValue({ rowCount: 2, rows: mockCategories });
+
+    const res = await dao.findAll();
+    expect(mockCategories).toEqual(res)
+  });
 });
