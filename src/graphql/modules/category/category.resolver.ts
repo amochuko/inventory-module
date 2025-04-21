8d8354eb20701e633409147dc260009080585343
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-21 13:23:19 +0100

    fix: update return value

--- src/graphql/modules/category/category.resolver.ts
+++ src/graphql/modules/category/category.resolver.ts
@@ -1,29 +1,16 @@
 import { GraphQLError } from "graphql";
 import { createLogger } from "graphql-yoga";
-import { getAbbrevationCodeFromName } from "../../../common/utils/helpers";
 import { DateScalar } from "../../types/custom-scalar";
 import { CategoryService } from "./category.service";
 import { CategoryModule } from "./generated-types/module-types";
 
 const logger = createLogger("debug");
 
-const categories = [
-  {
-    abbrevCode: "RAN-MET-01",
-    createdAt: new Date(),
-    description: "This is the rain",
-    id: "w234eweet2335",
-    name: "Rain Metal 01",
-    updatedAt: new Date(),
-  },
-];
-
 export const categoryResolvers: CategoryModule.Resolvers = {
   Query: {
     categories: async (_, args, ctx) => {
       logger.info("resolving categories");
-
-      return categories;
+     
     },
   },
   Mutation: {
@@ -31,12 +18,7 @@ export const categoryResolvers: CategoryModule.Resolvers = {
       logger.info("resolving create category");
 
       try {
-        const res = await ctx.injector.get(CategoryService).create({
-          ...argsObj,
-          abbrevCode: getAbbrevationCodeFromName(argsObj.name),
-        });
-
-        console.log({ res });
+        const res = await ctx.injector.get(CategoryService).create(argsObj);
 
         if (!res) {
           throw new GraphQLError(res, {
@@ -56,13 +38,7 @@ export const categoryResolvers: CategoryModule.Resolvers = {
           category: res,
         };
       } catch (err) {
-        if (err instanceof GraphQLError) {
-          throw err;
-        }
-
         logger.error("Unexpected error in resolver:", err);
-        // throw ErrorService.toGraphQLError(err);
-
         return {
           success: false,
           code: 400,
