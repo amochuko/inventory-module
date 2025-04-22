95d52b4e9fa9c50027be5dbbe570013243c1bbab
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-22 12:00:46 +0100

    update: add updateCategory and deleteCategory

--- src/graphql/modules/category/category.resolver.ts
+++ src/graphql/modules/category/category.resolver.ts
@@ -69,6 +69,56 @@ export const categoryResolvers: CategoryModule.Resolvers = {
         };
       }
     },
+    updateCategory: async (_, { id, body }, ctx) => {
+      try {
+        const res = await ctx.injector
+          .get(CategoryService)
+          .updateById(id, body as Partial<CategoryModule.Category>);
+
+        return {
+          success: true,
+          code: 200,
+          message: "Successfully update",
+          category: res,
+        };
+      } catch (err) {
+        logger.error(err);
+        return {
+          success: false,
+          code: 400,
+          message: "Not successfully updated",
+          category: null,
+        };
+      }
+    },
+    deleteCategoryById: async (_, args, ctx) => {
+      logger.info(`Attempting to delete category with id: ${args.id}`);
+
+      try {
+        await ctx.injector.get(CategoryService).deleteById(args.id);
+
+        logger.info(`Successfully deleted category with id: ${args.id}`);
+        return {
+          success: true,
+          code: 200,
+          message: "Successfully deleted",
+        };
+      } catch (err) {
+        logger.error("Error deleting category", err);
+
+        throw new GraphQLError(
+          "An unexpected error occurred while deleting the category.",
+          {
+            extensions: {
+              code: "DELETE_FAILED",
+              categoryId: args.id,
+              http: { status: 500 },
+              originalError: err,
+            },
+          }
+        );
+      }
+    },
   },
 
   Date: DateScalar,
