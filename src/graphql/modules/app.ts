7034c71508432901c284aa1d1066740a504e4ca7
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-23 15:36:41 +0100

    update: addproductModule and suppliermodule

--- src/graphql/modules/app.ts
+++ src/graphql/modules/app.ts
@@ -1,10 +1,12 @@
 import { createApplication } from "graphql-modules";
 import { PubSub } from "graphql-subscriptions";
 import { categoryModule } from "./category/category.module";
+import { productModule } from "./product/product.module";
+import { supplierModule } from "./supplier/suppiler.module";
 
 // This is the application, it contains GraphQL schema, and its implementation
 export const application = createApplication({
-  modules: [categoryModule],
+  modules: [categoryModule, productModule, supplierModule],
   providers: [
     {
       provide: PubSub,
