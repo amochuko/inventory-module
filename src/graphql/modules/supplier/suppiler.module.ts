8a873c6c274b6f118c1f907eb6b46e7f76725342
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-24 01:47:14 +0100

    fix: update providers list

--- src/graphql/modules/supplier/suppiler.module.ts
+++ src/graphql/modules/supplier/suppiler.module.ts
@@ -1,10 +1,15 @@
 import { createModule } from "graphql-modules";
+import { SupplierDAO } from "./supplier.dao";
+import { SupplierRepo } from "./supplier.repo";
+import { SupplierService } from "./supplier.service";
 import { supplierSchema } from "./typedefs/supplier.schema";
+import { supplierResolver } from "./supplier.resolver";
 
 export const supplierModule = createModule({
   id: "supplierModule",
   dirname: __dirname,
-  providers: [],
+  providers: [SupplierService, SupplierRepo, SupplierDAO],
   middlewares: {},
   typeDefs: [supplierSchema],
+  resolvers: supplierResolver
 });
