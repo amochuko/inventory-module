b61707306f69e8d2360abb9b57165aab9365e541
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-24 14:20:49 +0100

    fix: update thrown error

--- src/graphql/modules/supplier/supplier.dao.ts
+++ src/graphql/modules/supplier/supplier.dao.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "graphql-modules";
+import { createLogger } from "graphql-yoga";
+import { handlePostgresError } from "../../../config/database/dbErrorHandler";
 import { sql } from "../../../config/database/sqlConnection";
-import AppError, { ErrorCodes } from "../../../error/app.error";
 import { Supplier } from "../../generated-types/graphql";
 import { DAO, filterOptions } from "../interface/dao.interface";
+
+const logger = createLogger("error");
 
 export type CreateSupplierArgs = Omit<
   Supplier,
@@ -12,6 +15,8 @@ export type CreateSupplierArgs = Omit<
 @Injectable()
 export class SupplierDAO implements DAO<Supplier> {
   async create(args: CreateSupplierArgs): Promise<Supplier> {
+    logger.info(SupplierDAO.name, ": creating a supplier");
+
     try {
       const res = await sql({
         query: `INSERT INTO inventory.suppliers (name, description, email, address, phone)
@@ -26,36 +31,22 @@ export class SupplierDAO implements DAO<Supplier> {
         ],
       });
 
-      if (!res.rowCount) {
-        throw new AppError(
-          ErrorCodes.CREATION_FAILED,
-          {
-            ENTITY: "SUPPLIER",
-            categoryId: "234",
-          },
-          "Supplier not created!"
-        );
-      }
-
       return res.rows[0];
-    } catch (err) {
-      console.error(err);
-      if (err instanceof AppError) {
-        switch (err.code) {
-          case ErrorCodes.CREATION_FAILED:
-            throw "Failed to creaeted.";
+    } catch (err: any) {
+      logger.error(SupplierDAO.name, "DB error occurred", {
+        error: err,
+        stack: err.stack,
+        context: { entity: "SUPPLIER", operation: "create" },
+      });
 
-          default:
-            throw "Unknow error occured";
-        }
-      }
-      throw new Error("Error creating Supplier");
+      throw handlePostgresError(err, "SUPPLIER");
     }
   }
-  
-  findAll(args?: filterOptions): Promise<Supplier[]> {
+
+  async findAll(args?: filterOptions): Promise<Supplier[]> {
     throw new Error("Method not implemented.");
   }
+
   findById(id: string): Promise<Supplier> {
     throw new Error("Method not implemented.");
   }
