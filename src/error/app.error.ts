e0ae0fb60a899f8107c9e94009384a4906677ef9
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-24 01:41:02 +0100

    fix: update AppError

--- src/error/app.error.ts
+++ src/error/app.error.ts
@@ -1,5 +1,17 @@
+export enum ErrorCodes {
+  CREATION_FAILED = "CREATION_FAILED",
+  FETCH_FAILED = "FETCH_FAILED",
+  NOT_FOUND = "NOT_FOUND",
+  DUPLICATE = "DUPLICATE",
+  UNKNOWN = "UNKNOWN",
+}
+
 export default class AppError extends Error {
-  constructor(statusCode: number, message: string) {
+  constructor(
+    public code: ErrorCodes,
+    public context: Record<"ENTITY", Uppercase<string>> | Record<string, any>,
+    message: string
+  ) {
     super(message);
 
     this.name = this.constructor.name;
