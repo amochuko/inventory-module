c97322a18e2c000360cfde378feb02c8f4710043
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-24 01:51:50 +0100

    update: add module test

---
+++ src/graphql/modules/supplier/__tests__/supplier.module.test.ts
@@ -0,0 +1,14 @@
+import { Application, testkit } from "graphql-modules";
+import { supplierModule } from "../suppiler.module";
+
+describe("Testing supplierModule", () => {
+  let app: Application;
+
+  beforeEach(() => {
+    app = testkit.testModule(supplierModule, {});
+  });
+
+  test("schema is definded", () => {
+    expect(app.schema.getQueryType()).toBeDefined();
+  });
+});
