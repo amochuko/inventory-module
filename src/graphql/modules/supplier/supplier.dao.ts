0416f533ff582914e81d5c2f7dafabf3d4439747
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-25 16:30:05 +0100

    update: add findAll() with filter

--- src/graphql/modules/supplier/supplier.dao.ts
+++ src/graphql/modules/supplier/supplier.dao.ts
@@ -4,9 +4,10 @@ import { createLogger } from "graphql-yoga";
 import { handlePostgresError } from "../../../common/database/dbErrorHandler";
 import { sql } from "../../../common/database/sqlConnection";
 import { Supplier } from "../../generated-types/graphql";
-import { DAO, filterOptions } from "../interface/dao.interface";
+import { DAO } from "../interface/dao.interface";
+import { SupplierModule } from "./generated-types/module-types";
 
-const logger = createLogger("error");
+const logger = createLogger("debug");
 
 export type CreateSupplierArgs = Omit<
   Supplier,
@@ -44,8 +45,63 @@ export class SupplierDAO implements DAO<Supplier> {
     }
   }
 
-  async findAll(args?: filterOptions): Promise<Supplier[]> {
-    throw new Error("Method not implemented.");
+  async findAll(
+    args?: SupplierModule.SupplierFilterInput
+  ): Promise<Supplier[]> {
+    try {
+      const conditions: any[] = [];
+      let params: any[] = [];
+      let paramIndex = 1;
+
+      let query = "SELECT * FROM inventory.suppliers ";
+
+      if (args?.filter?.by && args?.filter?.by === "EMAIL") {
+        conditions.push(`email ILIKE ($${paramIndex++})`);
+        params.push(`${args.filter.term}%`);
+      }
+
+      if (args?.filter?.by && args?.filter?.by === "NAME") {
+        conditions.push(`name ILIKE ($${paramIndex++})`);
+        params.push(`${args.filter.term}%`);
+      }
+
+      if (args?.filter?.by && args?.filter?.by === "PHONE") {
+        conditions.push(`phone ILIKE ($${paramIndex++})`);
+        params.push(`${args.filter.term}%`);
+      }
+
+      if (conditions.length > 0) {
+        query += `WHERE ${conditions.join(" OR ")} `;
+      }
+
+      query += `ORDER BY id ASC `;
+
+      if (args?.filter?.skip) {
+        query += `OFFSET ($${paramIndex++}) `;
+        params.push(args.filter.skip);
+      }
+      if (args?.filter?.take) {
+        query += `LIMIT ($${paramIndex++}) `;
+        params.push(args.filter.take);
+      }
+
+      logger.info("findAll: ", { query, params });
+      
+      const res = await sql({ query, params });
+
+      if (res.rowCount && res.rowCount > 0) {
+        return res.rows;
+      }
+      return [];
+    } catch (err: any) {
+      logger.error(SupplierDAO.name, "DB error occurred", {
+        error: err,
+        stack: err.stack,
+        context: { entity: "SUPPLIER", operation: "findAll" },
+      });
+
+      throw handlePostgresError(err, "SUPPLIER");
+    }
   }
 
   findById(id: string): Promise<Supplier> {
