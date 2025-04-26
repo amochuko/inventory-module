7a37f5c12f7921a0da71068c15fed3067da2c0f1
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-26 12:46:45 +0100

    fix: update with individual error

--- src/common/database/dbErrorHandler.ts
+++ src/common/database/dbErrorHandler.ts
@@ -1,40 +1,30 @@
 import AppError, { ErrorCodes } from "../../error/app.error";
+import { DuplicateError } from "../../error/duplicate.error";
+import { NotFoundError } from "../../error/not-found.error";
 
 export function handlePostgresError(err: any, entity = "ENTITY") {
   const pgCode = err?.code;
   const message = err?.message || "";
 
   if (pgCode === "23505" || /duplicate key value/gi.test(message)) {
-    return new AppError(
-      ErrorCodes.DUPLICATE,
-      { ENTITY: entity, FIELD: "name" },
-      `${entity} with this name already exists.`
-    );
+    throw new DuplicateError(entity, ErrorCodes.DUPLICATE, 201);
   }
 
   if (pgCode === "23503") {
-    return new AppError(
-      ErrorCodes.NOT_FOUND,
-      { ENTITY: "RELATED_ENTITY" },
-      "Foreign key constraint failed. Related entity not found."
-    );
+    throw new NotFoundError({ resource: "RELATED_ENTITY" });
   }
 
   if (pgCode === "23502") {
-    return new AppError(
+    throw new DuplicateError(
+      "A required field is missing or null.",
       ErrorCodes.VALIDATION_ERROR,
-      { ENTITY: entity },
-      "A required field is missing or null."
+      201
     );
   }
 
   // Default fallback
   return new AppError(
-    ErrorCodes.SERVER_ERROR,
-    {
-      ENTITY: entity,
-      DETAIL: message,
-    },
-    `Unexpected error occurred while working with ${entity}.`
+    `Unexpected error occurred while working with ${entity}.`,
+    ErrorCodes.SERVER_ERROR
   );
 }
