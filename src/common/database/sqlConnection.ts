80aa30bc761892cc8a1d07b5eca58328cfbe1921
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-29 11:13:26 +0100

    update: add withTransaction

--- src/common/database/sqlConnection.ts
+++ src/common/database/sqlConnection.ts
@@ -53,4 +53,22 @@ export async function queryOne<T extends pg.QueryResultRow = any>(
   return result.rows[0] || null;
 }
 
+// Transaction support
+export async function withTransaction<T>(
+  fn: (client: pg.PoolClient) => Promise<T>
+): Promise<T> {
+  const client = await dbClient.connect();
 
+  try {
+    await client.query("BEGIN");
+    const result = await fn(client);
+    await client.query("COMMIT");
+
+    return result;
+  } catch (err) {
+    await client.query("ROLLBACK");
+    throw err;
+  } finally {
+    client.release();
+  }
+}
