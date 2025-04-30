60b24472e95849645862bab868774fda611b935a
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-30 11:49:34 +0100

    fix: update dbClient

--- src/common/database/sqlConnection.ts
+++ src/common/database/sqlConnection.ts
@@ -1,28 +1,27 @@
 import pg from "pg";
 import env from "../utils/env";
 
-export const dbClient = (() => {
-  if (process.env.NODE_ENV === "development") {
-    return new pg.Pool({
-      host: env.PGHOST_LOCAL,
-      user: env.PGUSER_LOCAL,
-      database: env.PGDATABASE_LOCAL,
-      password: env.PGPASSWORD_LOCAL,
-      port: env.PGPORT_LOCAL,
-      ssl: false,
-    });
-  } else if (process.env.NODE_ENV === "test") {
-    return new pg.Pool({
-      connectionString: env.POSTGRES_TEST_DATABASE_URL,
-    });
-  } else {
-    return new pg.Pool({
-      connectionString: env.PGDB_CONNECTION_STRING,
-    });
-  }
-})();
+let pool: pg.Pool;
 
-dbClient.on("error", (err) => {
+const nodeEnv = process.env.NODE_ENV;
+export const dbClient = {
+  getPool: () => {
+    if (!pool) {
+      pool = new pg.Pool({
+        connectionString:
+          nodeEnv === "test"
+            ? env.POSTGRES_TEST_DATABASE_URL
+            : nodeEnv === "development"
+            ? env.PGDB_DEV_CONNECTION_STRING
+            : env.PGDB_PRO_CONNECTION_STRING,
+      });
+    }
+
+    return pool;
+  },
+};
+
+dbClient.getPool().on("error", (err) => {
   console.log("Unexpected error on idle client: ", err);
   process.exit(-1);
 });
@@ -32,24 +31,21 @@ type SQLArgs = {
   params?: any[];
 };
 export async function sql<T extends pg.QueryResultRow = any>(args: SQLArgs) {
-  const client = await dbClient.connect();
+  const client = await dbClient.getPool().connect();
+  console.log("🔌 Client acquired");
 
   try {
     const result = await client.query<T>(args.text, args.params);
     return result;
   } catch (err) {
+    console.error("❌ Query error:", err);
+
     throw err;
   } finally {
     client.release();
+    console.log("✅ Client released");
   }
 }
-
-export async function shutdownDatabase() {
-  console.log("🧹 Calling dbClient.end()...");
-  await dbClient.end();
-  console.log("✅ Pool ended.");
-}
-
 
 // for fetching just one row
 export async function queryOne<T extends pg.QueryResultRow = any>(
@@ -64,7 +60,7 @@ export async function queryOne<T extends pg.QueryResultRow = any>(
 export async function withTransaction<T>(
   fn: (client: pg.PoolClient) => Promise<T>
 ): Promise<T> {
-  const client = await dbClient.connect();
+  const client = await dbClient.getPool().connect();
 
   try {
     await client.query("BEGIN");
