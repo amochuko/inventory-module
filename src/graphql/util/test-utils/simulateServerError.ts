639096ddc7eca0d01b7b54aa3772aafe25c9a8d2
amochuko <7204868+amochuko@users.noreply.github.com>
2025-04-30 11:50:16 +0100

    fix: remove param

--- src/graphql/util/test-utils/simulateServerError.ts
+++ src/graphql/util/test-utils/simulateServerError.ts
@@ -1,9 +1,11 @@
-import { PoolClient } from "pg";
+import { sql } from "../../../common/database/sqlConnection";
 import { ErrorCodes } from "../../../error/app.error";
 
-export async function simulateServerError(pool: PoolClient): Promise<void> {
+export async function simulateServerError(): Promise<void> {
   try {
-    await pool.query("SELECT * FROM totally_invalid_table_name");
+    await sql({
+      text: "SELECT * FROM totally_invalid_table_name",
+    });
   } catch (err) {
     throw new Error(ErrorCodes.SERVER_ERROR);
   }
