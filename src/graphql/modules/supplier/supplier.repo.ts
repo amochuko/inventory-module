18574d615905b07ba7898574497a00400ee8a8b3
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-01 11:26:55 +0100

    update: add supplier repo

---
+++ src/graphql/modules/supplier/supplier.repo.ts
@@ -0,0 +1,76 @@
+import { Injectable } from "graphql-modules";
+import { IDAO } from "../interface/dao.interface";
+import { SupplierModule } from "./generated-types/module-types";
+import { SupplierModel } from "./model/supplier.model";
+import { CreateSupplierArgs, SupplierDAO } from "./supplier.dao";
+
+@Injectable()
+export class SupplierRepo implements IDAO<SupplierModel> {
+  constructor(private dao: SupplierDAO) {}
+
+  async insert(args: CreateSupplierArgs): Promise<SupplierModel> {
+    const res = await this.dao.insert(args);
+
+    return SupplierModel.rebuildFromPersistence(res);
+  }
+
+  async findAll(
+    args?: SupplierModule.SupplierFilterInput
+  ): Promise<SupplierModel[]> {
+    const entities = await this.dao.findAll(args);
+
+    return entities.map(
+      (e) =>
+        new SupplierModel(
+          e.id,
+          e.name,
+          e.email,
+          e.address,
+          e.description,
+          e.phone,
+          e.created_at,
+          null
+        )
+    );
+  }
+
+  async findById(id: string): Promise<SupplierModel> {
+    const row = await this.dao.findById(id);
+
+    return SupplierModel.rebuildFromPersistence(row);
+  }
+
+  async updateById(
+    id: string,
+    supplier: Partial<SupplierModel>
+  ): Promise<SupplierModel> {
+    return await this.dao.updateById(id, supplier);
+  }
+
+  async save(supplier: SupplierModel): Promise<SupplierModel> {
+    const data = supplier.toPersistence();
+
+    if (!supplier.id) {
+      const created = await this.dao.insert(data);
+
+      if (!created) {
+        throw new Error("Failed to insert new supplier");
+      }
+
+      return SupplierModel.rebuildFromPersistence(created);
+    } else {
+      // update existing supplier
+      const updated = await this.dao.updateById(supplier.id, data);
+
+      if (!updated) {
+        throw new Error(`Failed to updated supplier with ID: ${supplier.id}`);
+      }
+
+      return SupplierModel.rebuildFromPersistence(updated);
+    }
+  }
+
+  async deleteById(id: string): Promise<boolean | null> {
+    return await this.dao.deleteById(id);
+  }
+}
