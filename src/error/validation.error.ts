e21c334b2b7fe995334299868fd3e4f16a3d868d
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-02 21:53:59 +0100

    fix: update with proper typing

--- src/error/validation.error.ts
+++ src/error/validation.error.ts
@@ -1,6 +1,24 @@
-export default class ValidationError extends Error {
-  constructor(msg: string) {
-    super(msg);
-    this.name = ValidationError.name;
+import AppError from "./app.error";
+import { ErrorCodes } from "./error.codes";
+
+// TODO: Update AppError to use this format
+type ValidationErrorOptions = {
+  extensions: {
+    code: ErrorCodes;
+    statusCode?: number;
+    errors: Record<string, any> | any[];
+  };
+};
+
+/**
+ * This is the ValidationError used to handle neccessary exception
+ */
+export default class ValidationError extends AppError {
+  constructor(
+    msg: string = "Invalid data provided",
+    options: ValidationErrorOptions
+  ) {
+    super(msg, options.extensions.code, options.extensions.statusCode);
+
   }
 }
