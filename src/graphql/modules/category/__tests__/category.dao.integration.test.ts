e1479ac232023b6ee5270bd5221073b0cc08d45a
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-05 19:16:53 +0100

    fix: upgrade to integration test

--- src/graphql/modules/category/__tests__/category.dao.integration.test.ts
+++ src/graphql/modules/category/__tests__/category.dao.integration.test.ts
@@ -1,140 +1,126 @@
-import { sql } from "../../../../common/database/sqlConnection";
+import { Category } from "../../../generated-types/graphql";
 import CategoryDAO from "../category.dao";
-import { CategoryNotFoundError } from "../category.error";
+import {
+  CategoryCreationError,
+  CategoryNotFoundError,
+} from "../category.error";
 
 const mockCategories = [
   {
-    id: "1",
+    id: 1,
     name: "Electronics",
     abbrev_code: "ELE-11",
     description: "Tech",
   },
   {
-    id: "2",
+    id: 2,
     name: "Food",
     abbrev_code: "FOD-4",
     description: "Feed house",
+  },
+  {
+    id: 3,
+    name: "Food Item",
+    abbrev_code: "FOD-9",
+    description: "Animal feed house",
   },
 ];
 
 describe("CategoryDAO", () => {
   const dao = new CategoryDAO();
 
-  it("should insert and retrieve category successfully", async () => {
-    const result = await dao.insert({
-      abbrev_code: "ELE-10",
-      description: "TECH",
-      name: "Electronic",
+  let result: Category;
+  beforeEach(async () => {
+    result = await dao.insert({
+      abbrev_code: mockCategories[0].abbrev_code,
+      description: mockCategories[0].description,
+      name: mockCategories[0].name,
     });
+  });
 
+  it("should insert and retrieve category successfully", async () => {
     expect(result).toEqual(
-      expect.objectContaining({ name: "Electronic", id: 1 })
+      expect.objectContaining({ name: "Electronics", id: 1 })
     );
   });
 
-  xit("should throw a creation error if insert fails", async () => {
-    (sql as jest.Mock).mockReturnValue({ rowCount: 0, rows: [] });
-
-    await expect(
-      dao.insert({ name: "Fail", abbrev_code: "FAIL", description: "none" })
-    ).rejects.toThrow("Failed to insert category");
-  });
-
-  xit("should detect duplicate constraint violation", async () => {
-    (sql as jest.Mock).mockRejectedValue(
-      new Error(
-        'duplicate key value violates unique constraint "categories_name_key"'
-      )
-    );
-
+  it("should detect duplicate constraint violation", async () => {
     await expect(
       dao.insert({
-        name: "Duplicate",
+        name: mockCategories[0].name,
         abbrev_code: "DUP",
         description: "Copy",
       })
     ).rejects.toThrow("Category name already exists");
   });
 
-  // describe("findAll", () => {
-  //   it("should return categories", async () => {
-  //     (sql as jest.Mock).mockResolvedValue({
-  //       rowCount: 2,
-  //       rows: mockCategories,
-  //     });
+  it("should throw a creation error if insert fails", async () => {
+    await expect(
+      dao.insert({ name: "", abbrev_code: "FAIL", description: "none" })
+    ).rejects.toThrow(CategoryCreationError);
+  });
 
-  //     const res = await dao.findAll();
-  //     expect(mockCategories).toEqual(res);
-  //   });
+  describe("findAll", () => {
+    it("should return categories", async () => {
+      const category = await dao.insert(mockCategories[1]);
 
-  //   it("should return categories with filter", async () => {
-  //     (sql as jest.Mock).mockResolvedValue({
-  //       rowCount: 2,
-  //       rows: mockCategories.slice(2),
-  //     });
+      const res = await dao.findAll();
+      expect(res).toEqual([result, category]);
+    });
 
-  //     const res = await dao.findAll({ filterByName: "food", skip: 0, take: 4 });
-  //     expect(res).toEqual(mockCategories.slice(2));
-  //   });
-  // });
+    it("should return categories with filter", async () => {
+      await dao.insert(mockCategories[1]);
+      await dao.insert(mockCategories[2]);
 
-  // describe("findById", () => {
-  //   it("should return a category by id", async () => {
-  //     (sql as jest.Mock).mockResolvedValue({
-  //       rowCount: 1,
-  //       rows: [mockCategories[0]],
-  //     });
+      const res = await dao.findAll({ filterByName: "food", skip: 0, take: 4 });
 
-  //     const category = await dao.findById("1");
-  //     expect(category).toEqual(mockCategories[0]);
-  //   });
+      expect(
+        res.map((r) => {
+          delete r.created_at;
+          delete r.updated_at;
+          return r;
+        })
+      ).toEqual(expect.objectContaining(mockCategories.slice(1)));
+    });
+  });
 
-  //   it("should throw if category by id is not in db", async () => {
-  //     (sql as jest.Mock).mockRejectedValue(new CategoryNotFoundError());
+  describe("findById", () => {
+    it("should return a category by id", async () => {
+      const category = await dao.findById("1");
 
-  //     await expect(dao.findById("5")).rejects.toThrow(CategoryNotFoundError);
-  //   });
-  // });
+      expect(category).toEqual(expect.objectContaining(mockCategories[0]));
+    });
 
-  // describe("updateById", () => {
-  //   const name = "Electronics and Gadgets";
+    it("should throw if category by id is not in db", async () => {
+      await expect(dao.findById("5")).rejects.toThrow(CategoryNotFoundError);
+    });
+  });
 
-  //   it("should update a category by id", async () => {
-  //     (sql as jest.Mock).mockResolvedValue({
-  //       rowCount: 1,
-  //       rows: [{ ...mockCategories[0], name }],
-  //     });
+  describe("updateById", () => {
+    const name = "Electronics and Gadgets";
 
-  //     const category = await dao.updateById("1", { name });
+    it("should update a category by id", async () => {
+      const category = await dao.updateById("1", { name });
 
-  //     expect(category.name).toEqual(name);
-  //   });
+      expect(category.name).toEqual("Electronics and Gadgets");
+    });
 
-  //   it("should throw if category by id is not in db", async () => {
-  //     (sql as jest.Mock).mockRejectedValue(new CategoryNotFoundError());
+    it("should throw if category by id is not in db", async () => {
+      await expect(dao.updateById("5", { name })).rejects.toThrow(
+        CategoryNotFoundError
+      );
+    });
+  });
 
-  //     await expect(dao.updateById("5", { name })).rejects.toThrow(
-  //       CategoryNotFoundError
-  //     );
-  //   });
-  // });
+  describe("deleteById", () => {
+    it("should delete a category by id", async () => {
+      const res = await dao.deleteById("1");
 
-  // describe("deleteById", () => {
-  //   it("should delete a category by id", async () => {
-  //     (sql as jest.Mock).mockResolvedValue({
-  //       rowCount: 1,
-  //       rows: [],
-  //     });
+      expect(res).toBeTruthy();
+    });
 
-  //     const res = await dao.deleteById("1");
-
-  //     expect(res).toBeTruthy();
-  //   });
-
-  //   it("should throw if category by id is not in db", async () => {
-  //     (sql as jest.Mock).mockRejectedValue(new CategoryNotFoundError());
-
-  //     await expect(dao.deleteById("5")).rejects.toThrow(CategoryNotFoundError);
-  //   });
-  // });
+    it("should throw if category by id is not in db", async () => {
+      await expect(dao.deleteById("5")).rejects.toThrow(CategoryNotFoundError);
+    });
+  });
 });
