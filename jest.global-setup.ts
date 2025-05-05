f0704c08c9f99b097243aeb2d7af05e4e6d2e39d
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-05 19:30:42 +0100

    fix: update with migration call

--- jest.global-setup.ts
+++ jest.global-setup.ts
@@ -1,21 +1,14 @@
 import dotenv from "dotenv";
-dotenv.config({ path: ".env.test" });
+dotenv.config({ path: "./.env.test" });
 
-import { sql } from "./src/common/database/sqlConnection";
+import { runDBMigration } from "./db-migrations";
+import env from "./src/common/utils/env";
+
+const testConnStr = env.PGDB_TEST_CONNECTION_STRING;
+const nodeEnv = process.env.NODE_ENV;
 
 export default async () => {
-  await sql({
-    text: `
-    DROP TABLE IF EXISTS inventory.suppliers CASCADE;
-      CREATE TABLE inventory.suppliers (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(50) NOT NULL UNIQUE,
-      address VARCHAR(255) NOT NULL,
-      email VARCHAR(100) NOT NULL,
-      phone VARCHAR(25) NOT NULL,
-      description character varying(255) NOT NULL,
-      created_at timestamp with time zone NOT NULL DEFAULT now(),
-      updated_at timestamp with time zone NOT NULL DEFAULT now()
-    )`,
-  });
+  console.log({ nodeEnv, testConnStr });
+
+  await runDBMigration();
 };
