86dd6b9f7cabbfda568095bd2e3f46abe1bcb99d
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-05 19:29:12 +0100

    update: add dynamic cleanup

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,16 +2,35 @@
  * This ensures tests don’t leak data between each other.
  */
 
-import dotenv from "dotenv";
-dotenv.config({ path: ".env.test" });
-
 import "reflect-metadata";
 import { sql } from "./src/common/database/sqlConnection";
 
+/**
+ * Dynamically truncates all tables in the `inventory` schema.
+ * This is helpful in automatically adapting to when new test tables
+ * are added without updating this function.
+ */
+export async function clearTestTable() {
+  const result = await sql({
+    text: `
+    SELECT tablename FROM pg_tables
+    WHERE schemaname = 'inventory' AND tablename != '_migrations';`,
+  });
+
+  const tables = result.rows.map((row) => `inventory.${row.tablename}`);
+
+  if (tables.length === 0) {
+    console.warn("No tables found to truncate.");
+    return;
+  }
+
+  console.log("[Tables to be migrated]:", tables.join(", "));
+  await sql({
+    text: `TRUNCATE TABLE ${tables.join(", ")} RESTART IDENTITY CASCADE;`,
+  });
+}
+
 beforeEach(async () => {
   console.log("⚠️ Truncating suppliers table...");
-
-  await sql({
-    text: "TRUNCATE TABLE inventory.suppliers RESTART IDENTITY CASCADE",
-  });
+  await clearTestTable();
 });
