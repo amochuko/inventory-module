5a987a8800c056abdbfab027ccf9bc7ccd6ff672
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-06 10:05:37 +0100

    fix: make config optional

--- src/error/app.error.ts
+++ src/error/app.error.ts
@@ -1,23 +1,23 @@
 import { ErrorCodes } from "./error.codes";
 
 // TODO: Update AppError to use this format
-export interface AppErrorOptions  {
+export interface AppErrorOptions {
   extensions: {
     code: ErrorCodes;
     statusCode?: number;
-    errors: Record<string, any>
+    errors: Record<string, any>;
   };
-};
+}
 
 export default class AppError extends Error {
   public code: ErrorCodes;
   public readonly name: string;
   public readonly statusCode: number;
 
-  constructor(message: string, options: AppErrorOptions) {
+  constructor(message: string, options?: AppErrorOptions) {
     super(message);
-    this.code = options.extensions.code;
-    this.statusCode = options.extensions.statusCode || 500;
+    this.code = options?.extensions.code || ErrorCodes.UNKNOWN;
+    this.statusCode = options?.extensions.statusCode || 500;
     this.name = this.constructor.name;
 
     Error.captureStackTrace(this, this.constructor);
