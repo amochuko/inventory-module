152a54dceb1b771d3035e1a1f2d81fd596580445
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-12 14:52:25 +0100

    update: add more integration test

--- src/graphql/modules/category/__tests__/category.module.test.ts
+++ src/graphql/modules/category/__tests__/category.module.test.ts
@@ -1,15 +1,70 @@
-import { Application, testkit } from "graphql-modules";
+import { Application, gql, testkit } from "graphql-modules";
+import { Category } from "../../../generated-types/graphql";
 import { categoryModule } from "../category.module";
- 
+
+type Result = { createCategory: Category };
 
 describe("Testing categoryModule", () => {
   let app: Application;
 
   beforeEach(() => {
-    app = testkit.testModule(categoryModule, {});
+    app = testkit.testModule(categoryModule, { replaceExtensions: true });
   });
 
   test("schema is definded", () => {
-    expect(app.schema.getQueryType()).toBeDefined();
+    const queryType = app.schema.getQueryType();
+
+    expect(queryType).toBeDefined();
+  });
+
+  it("should create a category and list it", async () => {
+    const createResult = await testkit.execute(app, {
+      document: gql`
+        mutation CreateCategory($input: CategoryCreateInput!) {
+          createCategory(argsObj: $input) {
+            success
+            message
+            category {
+              id
+              name
+            }
+          }
+        }
+      `,
+      variableValues: {
+        input: {
+          name: "Books",
+          description: "The books for all",
+        },
+      },
+    });
+
+    const createdCategory = createResult.data!.createCategory;
+
+    expect(createResult.errors).toBeUndefined();
+    expect(createdCategory?.category.name).toBe("Books");
+
+    const queryResult = await testkit.execute(app, {
+      document: gql`
+        query GetCategories($filter: CategoryFilterInput) {
+          categories {
+            id
+            name
+          }
+        }
+      `,
+      variableValues: {
+        filter: {},
+      },
+    });
+
+    expect(queryResult.errors).toBeUndefined();
+    expect(queryResult.data!.categories.length).toBe(1);
+    expect(queryResult.data!.categories).toEqual([
+      {
+        id: createdCategory?.category.id,
+        name: "Books",
+      },
+    ]);
   });
 });
