4b3bc4ab34c430ee4d05ea971864bc462c5a8c06
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-18 14:22:28 +0100

    fix: clean up

--- src/migrations/004_add_state_country_to_suppliers.sql
+++ src/migrations/004_add_state_country_to_suppliers.sql
@@ -2,4 +2,3 @@ ALTER TABLE inventory.suppliers
 ADD COLUMN state VARCHAR(100),
 ADD COLUMN country VARCHAR(100); 
 -- TODO: Add a table to poulate countries and states and add foreign key constraints to these columns   
-
