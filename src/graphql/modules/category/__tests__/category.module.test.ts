ee1dde953e482107b82cc11297ecab0baf8a183e
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-18 14:28:12 +0100

    update: add more test

--- src/graphql/modules/category/__tests__/category.module.test.ts
+++ src/graphql/modules/category/__tests__/category.module.test.ts
@@ -1,3 +1,4 @@
+import { ExecutionResult } from "graphql";
 import { Application, gql, testkit } from "graphql-modules";
 import { Category } from "../../../generated-types/graphql";
 import { categoryModule } from "../category.module";
@@ -17,54 +18,104 @@ describe("Testing categoryModule", () => {
     expect(queryType).toBeDefined();
   });
 
-  it("should create a category and list it", async () => {
-    const createResult = await testkit.execute(app, {
-      document: gql`
-        mutation CreateCategory($input: CategoryCreateInput!) {
-          createCategory(argsObj: $input) {
-            success
-            message
-            category {
+  describe("full CRUD", () => {
+    let createResult: ExecutionResult;
+    let createdCategory: any;
+
+    beforeEach(async () => {
+      createResult = await testkit.execute(app, {
+        document: gql`
+          mutation CreateCategory($input: CategoryCreateInput!) {
+            createCategory(argsObj: $input) {
+              success
+              message
+              category {
+                id
+                name
+              }
+            }
+          }
+        `,
+        variableValues: {
+          input: {
+            name: "Books",
+            description: "The books for all",
+          },
+        },
+      });
+
+      createdCategory = createResult.data!.createCategory as any;
+    });
+
+    it("should create a category and list it", async () => {
+      expect(createResult.errors).toBeUndefined();
+      expect(createdCategory?.category.name).toBe("Books");
+
+      const queryResult = await testkit.execute(app, {
+        document: gql`
+          query GetCategories($filter: CategoryFilterInput) {
+            categories {
               id
               name
             }
           }
-        }
-      `,
-      variableValues: {
-        input: {
-          name: "Books",
-          description: "The books for all",
+        `,
+        variableValues: {
+          filter: {},
         },
-      },
+      });
+
+      expect(queryResult.errors).toBeUndefined();
+      expect(queryResult.data!.categories.length).toBe(1);
+      expect(queryResult.data!.categories).toEqual([
+        {
+          id: createdCategory?.category.id,
+          name: "Books",
+        },
+      ]);
     });
 
-    const createdCategory = createResult.data!.createCategory;
-
-    expect(createResult.errors).toBeUndefined();
-    expect(createdCategory?.category.name).toBe("Books");
-
-    const queryResult = await testkit.execute(app, {
-      document: gql`
-        query GetCategories($filter: CategoryFilterInput) {
-          categories {
-            id
-            name
+    it("should return category with id", async () => {
+      const queryResult = await testkit.execute(app, {
+        document: gql`
+          query GetCategory($cat_id: String!) {
+            category(id: $cat_id) {
+              id
+              name
+            }
           }
-        }
-      `,
-      variableValues: {
-        filter: {},
-      },
+        `,
+        variableValues: {
+          cat_id: "1",
+        },
+      });
+
+      expect(queryResult.errors).toBeUndefined();
+      expect(queryResult.data?.category.name).toBe("Books");
     });
 
-    expect(queryResult.errors).toBeUndefined();
-    expect(queryResult.data!.categories.length).toBe(1);
-    expect(queryResult.data!.categories).toEqual([
-      {
-        id: createdCategory?.category.id,
-        name: "Books",
-      },
-    ]);
+    it("should update a category by id", async () => {
+      const updateResult = await testkit.execute(app, {
+        document: gql`
+          mutation UpdateCategory($update_input: CategoryUpdateInput!) {
+            updateCategory(args: $update_input) {
+              id
+              name
+              description
+            }
+          }
+        `,
+        variableValues: {
+          update_input: {
+            id: "1",
+            changes: { name: "Stationary" },
+          },
+        },
+      });
+
+      console.log({ updateResult: JSON.stringify(updateResult.data!.updateCategory, null, 2) });
+      expect(updateResult.errors).toBeUndefined();
+      // expect(updateResult.data?.updateCategory.name).toBe("Stationary");
+    });
   });
 });
