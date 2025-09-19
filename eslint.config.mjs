86e4bdb65f2584c0f7231741ed95cf9555270b52
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-19 09:39:22 +0100

    fix: add node options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-const cleanedBrowserGlobals = Object.fromEntries(Object.entries(globals.browser).map(([key, value]) => [key.trim(), value]))
+const cleanedBrowserGlobals = Object.fromEntries(
+  Object.entries(globals.browser).map(([key, value]) => [key.trim(), value])
+);
+const cleanedNodeGlobals = Object.fromEntries(
+  Object.entries(globals.node).map(([key, value]) => [key.trim(), value])
+);
 
 export default [
-  { languageOptions: { globals: cleanedBrowserGlobals } },
+  {
+    languageOptions: {
+      globals: { ...cleanedNodeGlobals, ...cleanedBrowserGlobals },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
