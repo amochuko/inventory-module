01a51b05e912ac1d50c525432f5082373456e121
amochuko <7204868+amochuko@users.noreply.github.com>
2025-09-19 09:26:50 +0100

    update: attach app as middleware

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,27 @@
-import { Server } from "http";
-import { createServer } from "node:http";
+import http from "node:http";
 import { debug } from "node:util";
 import "reflect-metadata";
 import { yoga } from "./graphql/yoga";
+import app from "./rest/app";
 
 const PORT = process.env.PORT || 4000;
 
-const server: Server = createServer(yoga);
-server.listen(PORT, () => {
-  console.info(`Server is running on http://localhost:${PORT}/graphql`);
-});
+if (process.env.NODE_ENV !== "production") {
+  const server = http.createServer(app);
 
-server.once("close", async () => {
-  await yoga.dispose();
-});
-
-process.on("SIGTERM", () => {
-  debug("SIGTERM signal received: closing HTTP server.");
-
-  server.close(() => {
-    debug("HTTP server closed");
+  server.listen(PORT, () => {
+    console.info(`Server is running on http://localhost:${PORT}/graphql`);
   });
-});
+
+  server.once("close", async () => {
+    await yoga.dispose();
+  });
+
+  process.on("SIGTERM", () => {
+    debug("SIGTERM signal received: closing HTTP server.");
+
+    server.close(() => {
+      debug("HTTP server closed");
+    });
+  });
+}
